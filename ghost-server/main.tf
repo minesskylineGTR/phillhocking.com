13e70a9b2f52e70e75239a0958521354224aa990
Brandon Nistor <brandon.nistor@open.com>
2020-01-09 18:57:19 -0800

    refactor for terraform .12.14 release

--- ghost-server/main.tf
+++ ghost-server/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "ghost" {
-  ami                     = "${data.aws_ami.ubuntu.id}"
+  ami                     = data.aws_ami.ubuntu.id
   instance_type           = "t2.small"
-  key_name                = "${var.key_pair_name}"
-  vpc_security_group_ids  = "${var.security_groups}"
+  key_name                = var.key_pair_name
+  vpc_security_group_ids  = var.security_groups
 
   tags = {
     Name = "ghost-server"
@@ -29,10 +29,10 @@ resource "aws_instance" "ghost" {
     ]
 
     connection  {
-      host          = "${self.public_ip}"
+      host          = self.public_ip
       type          = "ssh"
       user          = "ubuntu"
-      private_key   = "${file("${var.key_pair_location}")}"
+      private_key   = file("${var.key_pair_location}")
     }
 
     on_failure = "fail"
@@ -49,10 +49,10 @@ resource "aws_instance" "ghost" {
     ]
 
     connection {
-      host          = "${self.public_ip}"     
+      host          = self.public_ip
       type          = "ssh"
       user          = "ubuntu"
-      private_key   = "${file("${var.key_pair_location}")}"
+      private_key   = file("${var.key_pair_location}")
     }
 
     on_failure = "fail"
@@ -62,7 +62,7 @@ resource "aws_instance" "ghost" {
     inline = [
       "sudo chown -R ubuntu:ubuntu /var/www/ghost/",
       "cat <<FILEXXX > /var/www/ghost/core/server/config/env/config.production.json",
-      "${data.template_file.ghost-config.rendered}",
+      data.template_file.ghost-config.rendered,
       "FILEXXX",
       "cd /var/www/ghost/",
       "yarn global add knex-migrator",
@@ -71,10 +71,10 @@ resource "aws_instance" "ghost" {
     ]
 
     connection {
-      host          = "${self.public_ip}"
+      host          = self.public_ip
       type          = "ssh"
       user          = "ubuntu"
-      private_key   = "${file("${var.key_pair_location}")}"
+      private_key   = file("${var.key_pair_location}")
     }
 
     on_failure = "fail"
@@ -87,10 +87,10 @@ resource "aws_instance" "ghost" {
       "sudo touch /etc/nginx/sites-available/ghost && sudo chown ubuntu:ubuntu /etc/nginx/sites-available/ghost",
       "sudo touch /etc/systemd/system/ghost.service && sudo chown ubuntu:ubuntu /etc/systemd/system/ghost.service",
       "cat <<FILEXXX > /etc/nginx/sites-available/ghost",
-      "${data.template_file.nginx-site-config.rendered}",
+      data.template_file.nginx-site-config.rendered,
       "FILEXXX",
       "sudo cat <<FILEXXX > /etc/systemd/system/ghost.service",
-      "${data.template_file.service-config.rendered}",
+      data.template_file.service-config.rendered,
       "FILEXXX",
       "sudo ln -s /etc/nginx/sites-available/ghost /etc/nginx/sites-enabled/ghost",
       "sudo rm /etc/nginx/sites-enabled/default",
@@ -101,10 +101,10 @@ resource "aws_instance" "ghost" {
     ]
 
     connection {
-      host          = "${self.public_ip}"
+      host          = self.public_ip
       type          = "ssh"
       user          = "ubuntu"
-      private_key   = "${file("${var.key_pair_location}")}"
+      private_key   = file("${var.key_pair_location}")
     }
 
     on_failure = "fail"
